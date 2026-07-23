70003b38735e97dab4ca4c3101ee208e7aba23ea
Tran Ngoc Hieu Van <hieuvantnt@gmail.com>
2026-07-23 23:03:57 +0700

    Last commit of RScript file, RMarkdown file, and PDF file

--- A2_RScript.R
+++ A2_RScript.R
@@ -24,8 +24,8 @@ summary(teams)
 summary(tournaments)
 
 # Since only the matches dataset has missing values, replace "?" with NA
-matches[matches == "?"] <- NA
-str(matches) # Check if the missing values ? is replaced with NA
+matches[matches == "?"] <- NA # Replace the "?" values with "NA"
+str(matches) # Check if the missing values "?" are replaced with NA
 
 # Convert categorical variables to factors
 matches$Result <- as.factor(matches$Result)
@@ -51,19 +51,19 @@ sum(is.na(matches$AwayPenalty)) # Confirm no missing value in AwayPenalty
 library(dplyr) # Use the dplyr library for filtering
 unique(matches$PenaltyShootout) # Check the value of PenaltyShootout
 penalty_matches <- matches %>%
-  filter(PenaltyShootout == 1)
+  filter(PenaltyShootout == 1) # Filter out the penalty shootout matches
 penalty_matches # Check if the penalty_matches is filtered correctly
 
 # Since HomePenalty is a quantitative numeric variable, a histogram is 
 # selected for analyzing its distribution across matches involving a penalty shootout.
 table(penalty_matches$HomePenalty)
 library(ggplot2) # Use the ggplot2 library to produces the graphic with better customization
-ggplot(penalty_matches, aes(x = HomePenalty)) +
-  geom_histogram(binwidth = 1, fill = "lightblue", color = "black") +
-  labs(title = "Distribution of Home Penalty Goals in Penalty Shootout Matches",
-       x = "Number of Home Penalty Goals",
-       y = "Number of Matches") +
-  theme_minimal()
+ggplot(penalty_matches, aes(x = HomePenalty)) + # Create a ggplot object using the penalty_matches dataset and map HomePenalty to the x-axis
+  geom_histogram(binwidth = 1, fill = "lightblue", color = "black") + # Draw a histogram
+  labs(title = "Distribution of Home Penalty Goals in Penalty Shootout Matches", # Add a title for the histogram
+       x = "Number of Home Penalty Goals", # Add label for x-axis
+       y = "Number of Matches") + # Add label for y-axis
+  theme_minimal() # Apply a minimal theme for a clean and simple appearance
 # Interpretation: This histogram indicates that most home teams score 3 or 4 penalty goals during the penalty shootouts.
 # In contrast, lower scores (0-2 goals) and the maximum observed score (5 goals) occurred less often, suggesting
 # that home teams generally centered around a moderate number of penalties during shootouts.
@@ -78,14 +78,14 @@ matches_tournaments <- matches %>%
 str(matches_tournaments) # Validate if the TournamentName join successfully
 
 # Create histogram to analyze the distribution of HomeTeamScore across different competition stages.
-ggplot(matches, aes(x = HomeTeamScore)) +
-  geom_histogram(binwidth = 1, fill = "lightyellow", color = "black") +
+ggplot(matches, aes(x = HomeTeamScore)) + # Create a ggplot object using the matches dataset and map HomeTeamScore to the x-axis
+  geom_histogram(binwidth = 1, fill = "lightyellow", color = "black") + # Draw a histogram
   scale_x_continuous(breaks = seq(0, max(matches$HomeTeamScore), by = 1)) + # Display integer goals on the x-axis
   facet_wrap(~ Stage, scales = "free_y", ncol = 2) + # Compare score distribution across competition stages
-  labs(title = "Distribution of Home Team Scores across Competition Stages",
-       x = "Number of Goals Scored by the Home Team",
-       y = "Number of Matches") +
-  theme_minimal()
+  labs(title = "Distribution of Home Team Scores across Competition Stages", # Add a title for the histogram
+       x = "Number of Goals Scored by the Home Team", # Add label for x-axis
+       y = "Number of Matches") + # Add label for y-axis
+  theme_minimal() # Apply a minimal theme for a clean and simple appearance
 # Interpretation: The overall distribution of home team scores across competition stages is mainly concentrated around 1-2 scores.
 # Since the Group Stage contains the largest number of matches, it shows the widespread of scores and a right-skewed distribution,
 # indicating high-score performances from home team occurred occasionally. In contrast, the knockout stages, particularly final
@@ -93,14 +93,14 @@ ggplot(matches, aes(x = HomeTeamScore)) +
 # across the remaining knockout stages, most home teams scored between 1 and 3 goals, while scoring 5 goals or more were relatively uncommon.
 
 # Create histogram to analyze the distribution of HomeTeamScore across different tournaments
-ggplot(matches_tournaments, aes(x = HomeTeamScore)) +
-  geom_histogram(binwidth = 1, fill = "lightpink", color = "black") +
+ggplot(matches_tournaments, aes(x = HomeTeamScore)) + # Create a ggplot object using the matches_tournaments dataset and map HomeTeamScore to the x-axis
+  geom_histogram(binwidth = 1, fill = "lightpink", color = "black") + # Draw a histogram
   scale_x_continuous(breaks = seq(0, max(matches$HomeTeamScore), by = 1)) + # Display integer goals on the x-axis
   facet_wrap(~ TournamentName, scales = "free_y", ncol = 4) + # Compare score distribution across tournaments
-  labs(title = "Distribution of Home Team Scores across FIFA World Cup Tournaments",
-       x = "Number of Goals Scored by the Home Team",
-       y = "Number of Matches") +
-  theme_minimal()
+  labs(title = "Distribution of Home Team Scores across FIFA World Cup Tournaments", # Add a title for the histogram
+       x = "Number of Goals Scored by the Home Team", # Add label for x-axis
+       y = "Number of Matches") + # Add label for y-axis
+  theme_minimal() # Apply a minimal theme for a clean and simple appearance
 # Interpretation: Overall, the distribution of home team scores across FIFA World Cup tournaments is concentrated between 0 and
 # 3 goals, with most matches recording 1 to 2 goals. Additionally, later FIFA World Cup tournaments, particularly from 1998 onwards,
 # contain more matches that earlier editions, resulting in taller histograms while maintaining similar home team scoring distribution.
@@ -108,27 +108,27 @@ ggplot(matches_tournaments, aes(x = HomeTeamScore)) +
 
 # To categorize the AwayTeamScore across following groups: 0-1 goals, 2-3 goals, and 4 or more goals
 matches <- matches %>%
-  mutate(AwayScoreGroup = case_when(
-    AwayTeamScore <= 1 ~ "0-1 goals",
-    AwayTeamScore > 1 & AwayTeamScore <= 3 ~ "2-3 goals",
-    AwayTeamScore > 3 ~ "4+ goals"
+  mutate(AwayScoreGroup = case_when( # Add a new column based on conditions applied to AwayTeamScore
+    AwayTeamScore <= 1 ~ "0-1 goals", # Assign "0-1 goals" if the away team scored 0 or 1 goal
+    AwayTeamScore > 1 & AwayTeamScore <= 3 ~ "2-3 goals", # Assign "2-3 goals" if the away team scored 2 or 3 goal
+    AwayTeamScore > 3 ~ "4+ goals" # Assign "4+ goals" if the away team scored more than 3 goals
   ))
 # Convert AwayScoreGroup to an ordered factor for consistent group ordering in plots
 matches$AwayScoreGroup <- factor (
-  matches$AwayScoreGroup,
-  levels = c("0-1 goals", "2-3 goals", "4+ goals")
+  matches$AwayScoreGroup, # Specify the AwayScoreGroup variable to convert
+  levels = c("0-1 goals", "2-3 goals", "4+ goals") # Define the order of the factor levels from lowest to highest score group
 )
 # Validate the grouping of AwayScoreGroup before visualizing
-table(matches$AwayScoreGroup)
+table(matches$AwayScoreGroup) # Validate the grouping of AwayScoreGroup before visualizing
 # Create histogram to analyze the distribution of HomeTeamScore across AwayTeamScore groups
-ggplot(matches, aes(x = HomeTeamScore)) +
-  geom_histogram(binwidth = 1, fill = "lightgreen", color = "black") +
+ggplot(matches, aes(x = HomeTeamScore)) + # Create a ggplot object using the matches dataset and map HomeTeamScore to the x-axis
+  geom_histogram(binwidth = 1, fill = "lightgreen", color = "black") + # Draw a histogram
   scale_x_continuous(breaks = seq(0, max(matches$HomeTeamScore), by = 1)) + # Display integer goals on the x-axis
   facet_wrap(~ AwayScoreGroup, scales = "free_y") + # Compare score distribution across Away Team Score groups
-  labs(title = "Distribution of Home Team Scores by Away Team Score groups",
-       x = "Number of Goals Scored by the Home Team",
-       y = "Number of Matches") +
-  theme_minimal()
+  labs(title = "Distribution of Home Team Scores by Away Team Score groups", # # Add a title for the histogram
+       x = "Number of Goals Scored by the Home Team", # Add label for x-axis
+       y = "Number of Matches") + # Add label for y-axis
+  theme_minimal() # Apply a minimal theme for a clean and simple appearance
 # Interpretation: The distribution of goals scored by Home Team is similar across the three Away Team Score groups,
 # with most matches concentrated between 0 and 3 home goals regardless of the away team's scoring groups. From the distribution,
 # while the 0-1 goals contains significant number of observations, the 4+ goals contains fewer observations, indicating
@@ -176,15 +176,16 @@ stage_result_table <- table(matches$Stage, matches$Result) # Create frequency ta
 stage_result_table # View the table
 round(prop.table(stage_result_table, 1) * 100, 1) # View the table with percentage to support the analysis
 # Create a proportional stacked bar chart to compare match results across stages
-ggplot(matches, aes(x = Stage, fill = Result)) +
+ggplot(matches, aes(x = Stage, fill = Result)) + # Create a ggplot object using the matches dataset, mapping Stage to the x-axis and Result to the fill colour
   geom_bar(position = "fill") + # Draw stacked bars scaled to proportions (each bar sums to 100%) 
   scale_y_continuous(labels = scales::percent) + # Display the y-axis as percentages instead of proportions
-  labs(title = "Match Result Distribution by Competition Stage",
-       x = "Competition Stage",
-       y = "Percentage of Matches",
-       fill = "Result") +
-  theme_minimal()
-# Discussion on findings: The distribution of match result varies across different competition stages. Home team win accounts for
+  labs(title = "Match Result Distribution by Competition Stage", # Add title for the stacked bar chart
+       x = "Competition Stage", # Add label for x-axis
+       y = "Percentage of Matches", # Add label for y-axis
+       fill = "Result") + # Set the title of the legend
+  theme_minimal() + # Apply a minimal theme for a clean and simple appearance
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) # Rotate the x-axis labels by 30° and right-align them to improve readability.
+# Discussion on findings: The distribution of match result varies across different competition stages. Home team win accounts
 # for largest proportion of results in every stage. The final round and the Round of 16 show the highest proportion of home team
 # victories, with home team winning more than third quarters of the number of matches. In contrast, the Group Stage and Second
 # Group Stage have a noticeably higher proportions of draws more than knockout stages, indicating that draw matches are commonly
@@ -198,14 +199,14 @@ extratime_result_table <- table(matches$ExtraTime, matches$Result) # Create freq
 extratime_result_table # View the table
 round(prop.table(extratime_result_table, 1) * 100, 1) # View the table with percentage to support the analysis
 # Create a proportional stacked bar chart to compare match results by Extra Time
-ggplot(matches, aes(x = ExtraTime, fill = Result)) +
+ggplot(matches, aes(x = ExtraTime, fill = Result)) + # Create a ggplot object using the matches dataset, mapping ExtraTime to the x-axis and Result to the fill colour
   geom_bar(position = "fill") + # Draw stacked bars scaled to proportions (each bar sums to 100%)
   scale_y_continuous(labels = scales::percent) + # Display the y-axis as percentage instead of proportions
-  labs(title = "Match Result Distribution by Extra Time",
-       x = "Extra Time",
-       y = "Percentage of Matches",
-       fill = "Result") +
-  theme_minimal() 
+  labs(title = "Match Result Distribution by Extra Time", # Add title for the stacked bar chart
+       x = "Extra Time", # Add label for x-axis
+       y = "Percentage of Matches", # Add label for y-axis
+       fill = "Result") + # Set the title of the legend
+  theme_minimal() # Apply a minimal theme for a clean and simple appearance
 # Discussion on findings: The distribution of match outcomes has slightly difference depending on the match proceed extra time or
 # not. Matches with extra time recorded a lower proportion of draw and a higher proportion of away team wins compared with matches
 # within regular time. Additionally, the proportion of home team win is generally similar between two groups, making up more than 
